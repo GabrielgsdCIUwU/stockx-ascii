03572b1fed7f3db64f3f6c4854d930aed49974ea
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-17 14:51:27 +0100

    feat(bid): Añadir toString()

--- src/main/java/edu/estatuas/item/Bid.java
+++ src/main/java/edu/estatuas/item/Bid.java
@@ -1,7 +1,7 @@
 package edu.estatuas.item;
 
 public class Bid implements Offer {
-    
+
     private String size;
     private Integer bid;
 
@@ -24,5 +24,13 @@ public class Bid implements Offer {
     public int compareTo(Offer offer) {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t\t").append(size)
+                .append("\t").append(bid).append("\n");
+        return sb.toString();
     }
 }
