9a839da23da25db3730bb3e5141d5c2155f96dfd
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-19 10:54:54 +0100

    test(bid): Añadir caso test de Bid

--- src/test/java/edu/estatuas/BidTest.java
+++ src/test/java/edu/estatuas/BidTest.java
@@ -1,11 +1,21 @@
 package edu.estatuas;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import edu.estatuas.item.Bid;
 
 public class BidTest {
     
     @Test
     public void createBid() {
-        
+        String size = "18";
+        int bidValue = 200;
+
+        Bid bid = new Bid(size, bidValue);
+
+        assertEquals(size, bid.size());
+        assertEquals(bidValue, bid.value());
     }
 }
