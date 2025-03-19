3fbf41e5d9680036aaa9997bb2709f29413e0afa
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-19 10:55:48 +0100

    test(ask): Añadir caso test de crear Ask

---
+++ src/test/java/edu/estatuas/AskTest.java
@@ -0,0 +1,22 @@
+package edu.estatuas;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import edu.estatuas.item.Ask;
+
+public class AskTest {
+    
+    @Test
+    public void createAsk() {
+
+        String size = "13";
+        int askValue = 240;
+
+        Ask ask = new Ask(size, askValue);
+
+        assertEquals(size, ask.size());
+        assertEquals(askValue, ask.value());
+    }
+}
