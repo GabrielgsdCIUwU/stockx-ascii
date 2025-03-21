ec25fe2155e859b97d08d173577da8a792b527a2
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-21 08:38:10 +0100

    fix(minask): Añadir filtro para obtener solo de la misma clase Ask

--- src/main/java/edu/estatuas/criteria/MinAsk.java
+++ src/main/java/edu/estatuas/criteria/MinAsk.java
@@ -3,6 +3,7 @@ package edu.estatuas.criteria;
 import java.util.List;
 import java.util.Optional;
 
+import edu.estatuas.item.Ask;
 import edu.estatuas.item.Item;
 import edu.estatuas.item.Offer;
 
@@ -11,6 +12,7 @@ public class MinAsk implements Criteria {
     @Override
     public List<Offer> checkCriteria(Item item) {
         Optional<Offer> minOffer = item.offers().stream()
+            .filter(offer -> offer.getClass().equals(Ask.class))
             .min(Offer::compareTo);
 
         return minOffer.isPresent() ? List.of(minOffer.get()) : List.of();
